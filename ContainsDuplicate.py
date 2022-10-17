acca8cafff1032f7be0ad4a5db8b6367d92c1e6b
sor3nW <97264127+sor3nW@users.noreply.github.com>
2022-10-16 23:52:10 -0500

    Update ContainsDuplicate.py

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -9,4 +9,5 @@ def containsDuplicate(nums):
 #create a set 
 #sets cannot contain duplicates
 #for each number in nums check if the number has already been seen, and if it has then it is a duplicate so return True
-#if there are no duplicates then return False
+#if there are no duplicates then return False 
+#this is run in O(n) time while n is the length of array nums because the program has to run through the entire array 
